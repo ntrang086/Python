b137a4d0e2d4992954253dea6f6d1f883305004b
zrg <ntrang086@gmail.com>
2017-10-19 12:22:48 -0400

    Add another function to check anagrams using Counter

--- string/3 anagram.py
+++ string/3 anagram.py
@@ -3,13 +3,19 @@ def check_anagrams(s1, s2):
 	return sorted(s1) == sorted(s2)
 
 
-# Test the function above
+def check_anagrams2(s1, s2):
+	from collections import Counter
+	return Counter(s1) == Counter(s2)
+
+# Test the functions above
 s1 = 'nap'
 s2 = 'pan'
 s3 = 'abc'
 
 # Return true as s1 and as 2 are anagrams
 print (check_anagrams(s1, s2))
+print (check_anagrams2(s1, s2))
 
 # Return false as s1 and s3 are not anagrams
 print (check_anagrams(s1, s3))
+print (check_anagrams2(s1, s3))
