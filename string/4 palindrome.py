0ffbb41391c372b0316419e8832c30fff51f5096
zrg <ntrang086@gmail.com>
2017-10-20 13:21:50 -0400

    Add another function to check for palindromes without using reverse_string_iteratively function

--- string/4 palindrome.py
+++ string/4 palindrome.py
@@ -6,11 +6,20 @@ def reverse_string_iteratively(s):
 		i += 1
 	return reverse_s
 
-
+# Check if a string is a palindrome using reverse_string_iteratively function
 def is_palindrome(s):
 	return s == reverse_string_iteratively(s)
+
+# Another function to check if a string is a palindrome without using reverse_string_iteratively function
+def is_palindrome2(s):
+	for i in range(len(s)):
+		if s[i] != s[-i-1]:
+			return False
+	return True
 
 s1 = 'madam'
 s2 = 'abc'
 print (is_palindrome(s1))
 print (is_palindrome(s2))
+print (is_palindrome2(s1))
+print (is_palindrome2(s2))
