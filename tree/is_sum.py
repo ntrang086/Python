7daf3a51586f9af98e11229dae36d7fce47b973b
Trang Nguyen <ntrang086@gmail.com>
2018-02-20 15:11:59 -0500

    test: Add more tests

--- tree/is_sum.py
+++ tree/is_sum.py
@@ -1,7 +1,7 @@
 """Check if a tree is a sum tree"""
 
 from bst import BinarySearchTree
-from nary_tree import NArayTree
+from nary_tree import NArayTree, Node
 
 def get_subtree(current_node):
     subtree = [current_node]
@@ -38,33 +38,47 @@ if __name__ == "__main__":
         print ("Insert", element)
         bst.insert(element)
 
-    tree = bst.get_tree(bst.root)
     print ("\nPrint the tree in Depth First order")
     bst.print_tree()
     assert is_sum_tree(bst.root) == False
 
-    print ("Create a Binary Tree that is a sum tree")
-    sum_tree = NArayTree(is_binary=True)
+    assert is_sum_tree(None)
+
+    print ("Create a Binary Tree")
+    binary_tree = NArayTree(is_binary=True)
+    test_list = [15, 4, 7]
+    print ("Insert the elements from the following list:", test_list)
+    for element in test_list:
+        print ("Insert", element)
+        binary_tree.insert(element, binary_tree.root)
+    assert is_sum_tree(binary_tree.root) == False
+
+    print ("\nInsert another 4 as a child of node 4")
+    node_4 = binary_tree.root.left
+    node_4_new = Node(4, is_binary=True)
+    node_4.left = node_4_new
+    node_4.children.append(node_4_new)
+    node_4_new.parent = node_4
+    binary_tree.print_tree()
+    assert is_sum_tree(binary_tree.root)
+    assert is_sum_tree(node_4)
+    assert is_sum_tree(node_4_new)
+
+    print ("\nInsert 1 and 6 as children of node 7")
+    node_7 = binary_tree.root.right
+    node_1 = binary_tree.insert(1, node_7)
+    node_6 = binary_tree.insert(6, node_7)
+    binary_tree.print_tree()
+    assert is_sum_tree(binary_tree.root) == False
+    assert is_sum_tree(node_7)
+    assert is_sum_tree(node_6)
+
+
+    print ("Create another Binary Tree")
+    binary_tree = NArayTree(is_binary=True)
     test_list = [15, 8, 5, 2]
     print ("Insert the elements from the following list:", test_list)
     for element in test_list:
         print ("Insert", element)
-        sum_tree.insert(element, sum_tree.root)
-
-    tree = sum_tree.get_tree(sum_tree.root)
-    print ("\nPrint the tree in Depth First order")
-    sum_tree.print_tree()
-    assert is_sum_tree(sum_tree.root)
-
-    print ("\nInsert 2 and 6 as children of node 8")
-    node_8 = sum_tree.root.left
-    node_2 = sum_tree.insert(2, node_8)
-    node_6 = sum_tree.insert(6, node_8)
-    assert is_sum_tree(sum_tree.root) == False
-
-    print ("\nInsert 1 and 5 as children for node 6")
-    node_1 = sum_tree.insert(1, node_6)
-    assert is_sum_tree(sum_tree.root) == False
-    node_5 = sum_tree.insert(5, node_6)
-    assert is_sum_tree(node_6)
-
+        binary_tree.insert(element, binary_tree.root)
+    assert is_sum_tree(binary_tree.root) == False
