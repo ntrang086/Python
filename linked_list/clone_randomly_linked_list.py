72d00d97b8af1e261abbe551d9d4fc949625f0b9
Trang Nguyen <ntrang086@gmail.com>
2018-03-20 09:37:49 -0400

    test: Test clone_linked_list

--- linked_list/clone_randomly_linked_list.py
+++ linked_list/clone_randomly_linked_list.py
@@ -32,3 +32,28 @@ def clone_linked_list(linked_list):
         current_node = current_node.next
     return clone
 
+
+if __name__ == "__main__":
+    # Initialize a randomly linked list with a head that has key 1
+    rand_linked_list = DoublyLinkedList(Node(1))
+    rand_linked_list.append(2)
+    rand_linked_list.append(3)
+    rand_linked_list.append(4)
+    rand_linked_list.append(5)
+
+    # Assign the head's previous pointer to 3
+    rand_linked_list.head.previous = rand_linked_list.head.next.next
+    # Append node 3's previous pointer to 5
+    rand_linked_list.head.next.next.previous = rand_linked_list.head.next.next.next.next
+    # Assign node 5's previous pointer to 2 
+    rand_linked_list.head.next.next.next.next.previous = rand_linked_list.head.next
+    
+    clone = clone_linked_list(rand_linked_list)
+    # Should print 1, 2, 3, 4 and 5 respectively
+    clone.print_linked_list()
+    # Should print 3
+    print (clone.head.previous.key)
+    # Should print 5
+    print (clone.head.next.next.previous.key)
+    # Should print 2
+    print (clone.head.next.next.next.next.previous.key)
