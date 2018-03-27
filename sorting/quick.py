e55776d870914a22b0e40e02c6a909b2791c4b16
Trang Nguyen <ntrang086@gmail.com>
2018-03-27 13:23:15 -0400

    feat: Implement quick_sort based on Lomuto partition scheme

--- sorting/quick.py
+++ sorting/quick.py
@@ -2,6 +2,20 @@
 https://en.wikipedia.org/wiki/Quicksort
 """
 
+def quick_sort_lomuto(array, low, high):
+    """Divide a large array into two smaller sub-arrays: the low 
+    items and the high items; then recursively sort the sub-arrays.
+    """
+
+    # Base case: a list with 1 or 0 item is sorted
+    if len(array) <= 1:
+        return array
+    if low < high:
+        p = partition_lomuto(array, low, high)
+        quick_sort_lomuto(array, low, p - 1)
+        quick_sort_lomuto(array, p + 1, high)
+    return array
+
 def partition_lomuto(array, low, high):
     """Choose a pivot that is the last item in the array.
     Reorder the array so that all items with values less than
@@ -22,3 +36,8 @@ def partition_lomuto(array, low, high):
             i += 1
     array[i], array[high] = array[high], array[i]
     return i
+
+
+if __name__ == "__main__":
+    array = [6, 5, 3, 1, 8, 7, 2, 4]
+    print (quick_sort_lomuto(array, 0, len(array) - 1))
