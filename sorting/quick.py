efa9ccf3c86dbdd3ea60265ad6fe2c655dfc901c
Trang Nguyen <ntrang086@gmail.com>
2018-03-31 07:10:03 -0400

    feat: Implement two equivalent methods for partitioning an array using median_of_three as a pivot

--- sorting/quick.py
+++ sorting/quick.py
@@ -70,6 +70,58 @@ def partition_hoare(array, low, high):
             return j
         array[i], array[j] = array[j], array[i]
 
+def partition(array, low, high):
+    """Use two indices i and j that start at the ends of the array,
+    then move toward each other, until they detect an inversion: 
+    a pair of items, one greater than or equal to the pivot, one 
+    lesser or equal, that are in the wrong order relative to each 
+    other. The inverted items are then swapped. When the indices
+    meet, return the final index.
+    """
+
+    # Pick a pivot
+    pivot = median_of_three(array, low, high)
+    i = low
+    # j starts at high - 2 because thanks to the last swap in 
+    # median_of_three() array[high] is guaranteeed to be >= pivot and 
+    # array[high - 1] is the median itself, which will be put to the 
+    # correct position at the end of the below outer while loop
+    j = high - 2
+    while True:
+        while array[i] < pivot:
+            i += 1
+        while array[j] > pivot:
+            j -= 1
+        if i >= j:
+            return j
+        array[i], array[j] = array[j], array[i]
+
+def partition2(array, low, high):
+    """This is equivalent to partition(array, low, high)
+    """
+
+    # Pick a pivot
+    pivot = median_of_three(array, low, high)
+    i = low
+    # j starts at high - 2 because thanks to the last swap in 
+    # median_of_three() array[high] is guaranteeed to be >= pivot and 
+    # array[high - 1] is the median itself, which will be put to the 
+    # correct position after the below outer while loop
+    j = high - 2
+    while True:
+        while array[i] < pivot:
+            i += 1
+        while array[j] > pivot:
+            j -= 1
+        if i >= j:
+            break
+        else:
+            array[i], array[j] = array[j], array[i]
+    # Put the pivot to the correct position, i.e. all
+    # items on its left <= pivot, all on the right > pivot
+    array[i], array[high - 1] = array[high - 1], array[i]
+    return i
+
 def median_of_three(array, low, high):
     """Find the median of the left-most, right-most and center 
     items of an array
@@ -85,6 +137,7 @@ def median_of_three(array, low, high):
         array[high], array[low] = array[low], array[high]
     
     # Swap the pivot array[center] with array[high - 1]
+    # to make the partition more efficient
     array[center], array[high - 1] = array[high - 1], array[center]
     return array[high - 1]
 
