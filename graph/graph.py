34826b1a146e8fbf20f0666fccae87cebf2b4aef
Trang Nguyen <ntrang086@gmail.com>
2018-05-15 18:50:03 -0400

    feat: Implement breadth_first_traversal, depth_first_traversal, clear

--- graph/graph.py
+++ graph/graph.py
@@ -1,6 +1,7 @@
 """Create a directed graph that has nodes and edges. The Graph class has
 functions that return various representations of the same graph."""
 
+from collections import deque
 
 class Node(object):
     """A node has a value and a list of edges that start or end with it."""
@@ -11,7 +12,7 @@ class Node(object):
 
 
 class Edge(object):
-    """An edge has a value and a direction: the start node the end node."""
+    """An edge has a value and a direction: the start node and the end node."""
     def __init__(self, value, node_from, node_to):
         self.value = value
         self.node_from = node_from
@@ -19,9 +20,10 @@ class Edge(object):
 
 
 class Graph(object):
-    """A graph contains a list of vertexes and edges. It has functions to
-    insert/delete a node or an edge and to return various representations.
-    Assume that each node has a unique value, but edges do not have to."""
+    """A directed graph contains a list of vertexes and edges. It may have
+    cycles. It has functions to insert/delete a node or an edge, return various
+    representations and traverse the nodes. Assume that each node has a unique
+    value, but edges do not have to."""
 
     def __init__(self, nodes=[], edges=[]):
         self.nodes = nodes
@@ -58,20 +60,18 @@ class Graph(object):
         node_to.edges.append(new_edge)
         self.edges.append(new_edge)
 
-    def get_node_list(self):
-        """Return a list of node values"""
-        return [node.value for node in self.nodes]
-    
     def get_edge_list(self):
         """Return a list of triples that store an edge's value, "from" node and
-        "to" node: [(Edge Value, From Node Value, To Node Value), ...]"""
+        "to" node: [(Edge Value, From Node Value, To Node Value), ...].
+        """
         return [(edge.value, edge.node_from.value, edge.node_to.value) 
                 for edge in self.edges]
 
     def get_adjacency_list(self):
         """Return a list of lists: The indicies of the outer list represent
         "from" nodes. Each sublist can be None or store a list of tuples:
-        [[(To Node, Edge Value), (To Node, Edge Value), ...], None, ...]"""
+        [[(To Node, Edge Value), (To Node, Edge Value), ...], None, ...].
+        """
         max_index = max(node.value for node in self.nodes)
         adjacency_list = [None] * (max_index + 1)
         for edge in self.edges:
@@ -83,7 +83,8 @@ class Graph(object):
 
     def get_adjacency_matrix(self):
         """Return a matrix: Rows represent "from" nodes; columns represent "to"
-        nodes. Store an edge value in each spot, or a 0 if no edge exists."""
+        nodes. Store an edge value in each spot, or a 0 if no edge exists.
+        """
         max_index = max(node.value for node in self.nodes)
         # Initialize a matrix. Note that we can't use * for to create a list of
         # sublists because this will create a list containing (max_index + 1) 
@@ -92,6 +93,51 @@ class Graph(object):
         for edge in self.edges:
             adjacency_matrix[edge.node_from.value][edge.node_to.value] = edge.value
         return adjacency_matrix
+
+    def breadth_first_traversal(self, node_val):
+        """Traverse the graph in breadth-first order starting from node_val."""
+        # Keep track of nodes that we have seen
+        seen = set()
+        # A queue to store the frontier nodes to traverse
+        queue = deque()
+        result = []
+        start_node = self.search_node(node_val)
+        if start_node:
+            queue.append(start_node)
+            seen.add(start_node)
+        while queue:
+            node = queue.popleft()
+            result.append(node.value)
+            for edge in node.edges:
+                # If the node of interest is the start of an edge
+                if node is edge.node_from:
+                    next_node = edge.node_to
+                    if next_node not in seen:
+                        queue.append(next_node)
+                        seen.add(next_node)
+        return result
+    
+    def depth_first_traversal(self, node_val):
+        """Traverse the graph in depth-first order starting from node_val."""
+        return self._depth_first_traversal_util(node_val, set(), [])
+
+    def _depth_first_traversal_util(self, node_val, seen, result):
+        node = self.search_node(node_val)
+        if node:
+            result.append(node.value)
+            seen.add(node)
+        for edge in node.edges:
+            if node is edge.node_from:
+                next_node = edge.node_to
+                if next_node not in seen:
+                    self._depth_first_traversal_util(next_node.value, seen, result)
+        return result
+
+    def clear(self):
+        """Clear all nodes and edges. Otherwise, when creating a new graph, the
+        nodes and edges of an old graph may be added to the new one."""
+        self.nodes.clear()
+        self.edges.clear()
 
 
 if __name__ == "__main__":
@@ -102,10 +148,34 @@ if __name__ == "__main__":
     graph.insert_edge(102, 1, 4)
     graph.insert_edge(103, 3, 4)
 
-    # Test the functions of Graph
-    assert graph.get_node_list() == [1, 2, 3, 4]
+    # Test the graph representation functions
     assert graph.get_edge_list() == [(100, 1, 2), (101, 1, 3), (102, 1, 4), (103, 3, 4)]
     assert graph.get_adjacency_list() == [None, [(2, 100), (3, 101), (4, 102)], None, 
                                           [(4, 103)], None]
     assert graph.get_adjacency_matrix() == [[0, 0, 0, 0, 0], [0, 0, 100, 101, 102], 
                                             [0, 0, 0, 0, 0], [0, 0, 0, 0, 103], [0, 0, 0, 0, 0]]
+    # Test breadth-first traversal
+    assert graph.breadth_first_traversal(1) == [1, 2, 3, 4]
+    # Add a cycle for node 2
+    graph.insert_edge(100, 2, 2)
+    assert graph.breadth_first_traversal(1) == [1, 2, 3, 4]
+
+    # Test depth-first traversal
+    #node_1 = graph.nodes[0]
+    assert graph.depth_first_traversal(1) == [1, 2, 3, 4]
+
+    graph.clear()
+
+    # Create another graph
+    graph_2 = Graph()
+    graph_2.insert_edge(1, 0, 1)
+    graph_2.insert_edge(1, 0, 2)
+    graph_2.insert_edge(1, 1, 2)
+    graph_2.insert_edge(1, 2, 0)
+    graph_2.insert_edge(1, 2, 3)
+    graph_2.insert_edge(1, 3, 3)
+
+    # Test breadth-first traversal
+    assert graph_2.breadth_first_traversal(2) == [2, 0, 3, 1]
+    # Test depth-first traversal
+    assert graph_2.depth_first_traversal(2) == [2, 0, 1, 3]
