7a03314d101c37f2c6a096188689ac8518461895
Trang Nguyen <ntrang086@gmail.com>
2018-06-20 11:32:40 -0400

    feat: Combine all the distance functions into one

--- ai/knn.py
+++ ai/knn.py
@@ -1,35 +1,57 @@
-"""Implement k-nearest neighbors."""
+"""Implement k-nearest neighbors.
+1) https://members.loria.fr/MOBerger/Enseignement/Master2/Exposes/beyer.pdf
+describes which situations KNN works or doesn't work.
+
+2) https://homes.cs.washington.edu/~pedrod/papers/cacm12.pdf
+If a constant number of examples is distributed uniformly in a 
+high-dimensional hypercube, beyond some dimensionality most examples are closer
+to a face of the hypercube than to their nearest neighbor. And if we 
+approximate a hypersphere by inscribing it in a hypercube, in high dimensions
+almost all the volume of the hypercube is outside the hypersphere.
+
+3) https://bib.dbvis.de/uploadedFiles/155.pdf
+The Manhattan distance metric (L1) is the most preferable for high 
+dimensional applications, followed by the Euclidean metric (L2).
+"""
 
 import math
 from collections import Counter
 
 def classify_knn(points, p, k=3):
-    """Classify a point in a two-dimensional space using KNN.
+    """Classify a point using KNN.
     Parameters:
     points: Dictionary of training points having keys indicating classes.
              Each key have a list of training data points belong to it
     p: Test data point in the form of (x,y)
     k: Number of nearest neighbours to consider, default is 3
-    This paper https://members.loria.fr/MOBerger/Enseignement/Master2/Exposes/beyer.pdf
-    says that the Manhattan distance metric (L1) is the most preferable for high 
-    dimensional applications, followed by the Euclidean metric (L2).
     """
     distances = []
     for key in points:
         for point in points[key]:
-            distance = manhattan_distance(point, p)
+            # Compute Manhattan distance
+            distance = calculate_distance(point, p, 1)
             distances.append((distance, key))
     top_k_dist = sorted(distances)[:k]
     counts = Counter(x[1] for x in top_k_dist)
     return max(counts, key=lambda x: counts[x])
-    
-def euclidean_distance(a, b):
-    """Calculate the Euclidean distance between two points a and b."""
-    return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
 
-def manhattan_distance(a, b):
-    """Calculate the Manhattan distance between two points a and b."""
-    return (abs(a[0] - b[0]) + abs(a[1] - b[1]))
+def calculate_distance(a, b, k=1/2):
+    """Calculate the distance between two points a and b.
+    k = 1: Manhattan distance
+    k = 2: Euclidean distance
+    k < 1: fractional distance, proposed by the "Surprising Behavior" paper 
+    (https://bib.dbvis.de/uploadedFiles/155.pdf). "Fractional norms" exhibit 
+    the property of increasing the contrast between farthest and nearest 
+    points. This may be useful in some contexts, however there is a caveat:
+    these "fractional norms" are not proper distance metrics because they
+    violate the triangle inequality. If the triangle inequality is an important
+    quality to have in your research, then fractional metrics are not going
+    to be tremendously useful. (https://stats.stackexchange.com/a/99191)
+    """
+    result = 0
+    for i in range(len(a)):
+        result += math.pow(abs(a[i] - b[i]), k)
+    return math.pow(result, 1/k)
 
 
 if __name__ == "__main__":
